9c69d7e96c6f42e9eb293788c7f6dacc4c390a79
Sean Hong(홍성민) <seanhong2000@gmail.com>
2021-10-20 07:17:24 +0900

    Update README.md | Add detailed comments

--- README.md
+++ README.md
@@ -2,3 +2,60 @@
 
 Creating a docker-like container service from scratch using Go
 
+Go를 사용한 도커와 비슷한 컨테이너 서비스 시작부터 만들기
+## 시작하는 방법
+해당 코드는 우분투 20.04 lts 에서 확인하였습니다.
+
+```sh
+    # 해당 저장소를 클론 합니다.
+    git clone https://github.com/KKodiac/containers-from-scratch.git
+    # main.go 를 빌드 후 executable 생성합니다.
+    go build main.go 
+    # 루트 권한으로 실행합니다.
+    # 컨테이너 프로세스를 bash쉘로 모니터 합니다.
+    sudo ./main run /bin/bash
+```
+
+## namespace 
+[도커](https://docs.docker.com/get-started/overview/#the-underlying-technology) 등 컨테이너는 `namespace`라는 기법을 활용해서 각 컨테이너에게 독립된 환경설정을 제공합니다. 
+
+자세한 것은 [코드]()를 확인해주세요.
+
+## 컨테이너 확인
+위 처럼 시작 하였으면 bash로 컨테이너 루트(`/`)로 설정된 디렉토리에서 시작하게 됩니다.
+해당 컨테이너 쉘의 호스트 이름은 `container`로 설정되어 있습니다.
+```sh
+    # root@container:/#
+    ls # '/' 디렉토리 내용 출력
+```
+
+
+## 최대 프로세스 제한 
+해당 컨테이너는 생성할 수 있는 최대 프로세스가 `20`으로 제한되어 있습니다.
+```sh
+    # 컨테이너 프로세스를 실행합니다.
+    sudo ./main run /bin/bash
+```
+```sh
+    # Fork Bomb을 생성해서 확인해 봅니다. 무한의 프로세스 포크를 생성합니다.
+    # 컨테이너 내부에서 실행해야 됩니다! 호스트 쉘에서 실행하면... ㅎㅇㅌ...
+    root@container:/ :(){ :|: & }; :
+```
+
+호스트 OS(Linux Ubuntu)에서 확인하면,
+```sh
+    cd /sys/fs/cgroup/pids/ubuntu 
+    cat pids.max # 20
+    # 호스트에서 cgroup/pids/ubuntu 를 확인해보면,
+    # 컨테이너로 인해 생성된 프로세스가 그렇게 많지 않은 것을 확인할 수 있습니다. (20개)
+    ps aux 
+```
+
+## /proc 내용 확인
+시스템 내부의 프로세스 ID, 자원 등을 확인할 수 있습니다.
+
+호스트에서 `mount` 한 /proc 디렉토리를 확인합니다.
+```sh
+    ls /proc # 프로세스 ID 등등
+    ps # 컨테이너 내부의 프로세스 리스트
+```
--- main.go
+++ main.go
@@ -24,10 +24,21 @@ func main() {
 func run() {
 	fmt.Printf("Running %v \n", os.Args[2:])
 
+	// 자기 자신의 자식 프로세스 생성
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	// 쉘 기본 IO 추가
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
+	// Cloneflags 는 오직 리눅스에서 가능합니다
+	// 아래는 컨테이너 namespace를 설정해 줍니다.
+	// CLONE_NEWUTS 독립된 hostname용 namespace
+	// CLONE_NEWPID 독립된 process용 namespace
+	// CLONE_NEWNS 독립된 mounts용 namespace
+	// 자세한 내용은 아래 확인
+	// https://pkg.go.dev/syscall#pkg-constants
+	// https://pkg.go.dev/syscall#SysProcAttr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 		Unshareflags: syscall.CLONE_NEWNS,
@@ -37,32 +48,40 @@ func run() {
 }
 
 func child() {
-	fmt.Printf("Running %v \n", os.Args[2:])
+	fmt.Printf("Running %v as Child\n", os.Args[2:])
 	cg()
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	// 쉘 기본 IO 설정
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
+	// 컨테이너 호스트 이름을 'container' 로 설정
 	must(syscall.Sethostname([]byte("container")))
+	// 루트 디렉토리 설정을 호스트의 /home/ubuntu/ubuntufs 에서 가져와 설정
 	must(syscall.Chroot("/home/ubuntu/ubuntufs"))
+	// 컨테이너 쉘 시작을 '/' 로 설정
 	must(os.Chdir("/"))
+	// ps 명령어가 컨테이너에서 돌아가도록 /proc 마운트
 	must(syscall.Mount("proc", "proc", "proc", 0, ""))
-	// must(syscall.Mount("thing", "mytemp", "tmpfs", 0, ""))
+	// 임시 파일시스템 마운트
+	// must(syscall.Mount("thing", "mytemp", "tmpfs", 0, "")) // file not found error
 
 	must(cmd.Run())
 
+	// 마운트 클린업
 	must(syscall.Unmount("proc", 0))
 	must(syscall.Unmount("thing", 0))
 }
 
+// 호스트의 control group 패스에 설정 초기값 설정
 func cg() {
-	cgroups := "/sys/fs/cgroup/"
+	cgroups := "/sys/fs/cgroup/" // 리눅스 cgroup 폴더 위치
 	pids := filepath.Join(cgroups, "pids")
 	os.Mkdir(filepath.Join(pids, "ubuntu"), 0755)
-	must(ioutil.WriteFile(filepath.Join(pids, "ubuntu/pids.max"), []byte("20"), 0700)) // set max number of pids to 20
-	must(ioutil.WriteFile(filepath.Join(pids, "ubuntu/notify_on_release"), []byte("1"), 0700))
-	must(ioutil.WriteFile(filepath.Join(pids, "ubuntu/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
+	must(ioutil.WriteFile(filepath.Join(pids, "ubuntu/pids.max"), []byte("20"), 0700))                          // 컨테이너 내 최대 지정 가능 PID를 20개로 설정
+	must(ioutil.WriteFile(filepath.Join(pids, "ubuntu/notify_on_release"), []byte("1"), 0700))                  // cgroup 미사용 시 클린업
+	must(ioutil.WriteFile(filepath.Join(pids, "ubuntu/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700)) // cgroup 내 있는 모든 프로세스에 적용
 }
 
 func must(err error) {
